de3aea48f47576e4242935b0c343674f3b03eb23
Mohammed-Q96 <mhmmade@gmail.com>
2019-07-30 16:53:34 +0300

    app.js #6

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { join } = require('path');
+const compression = require('compression');
+
+const app = express();
+
+app.disable('x-powerd-by');
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
+
+module.exports = app;
