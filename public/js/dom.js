828ef436a51354397dd0b5e14e77bd66642ba04c
fadi omar <fadeomar2015@gmail.com>
2019-07-31 16:34:36 +0300

    make-dom-manibulation-and-css-style Relates #17

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
 // import { promises } from "dns";
 
 const input = document.querySelector('.input-feild');
 
 const btn = document.querySelector('.btn');
 
-// const result = document.querySelector('.result');
+const result = document.querySelector('.result');
 
 btn.addEventListener('click', () => {
   const word = JSON.stringify({ input: input.value });
@@ -19,6 +21,20 @@ btn.addEventListener('click', () => {
       return res.results;
     })
     .then(res => res.forEach((e) => {
-      console.log(e.title);
+	  console.log(e.title);
+	  const poster = document.createElement('div');
+	  const posterImg = document.createElement('img');
+	  const posterTitle = document.createElement('h2');
+	  const posterOverview = document.createElement('P');
+	  poster.classList.add('poster');
+	  posterImg.classList.add('posterImg');
+	  posterTitle.classList.add('posterTitle');
+	  posterImg.setAttribute('src', `https://image.tmdb.org/t/p/w500/${e.poster_path}`);
+	  poster.appendChild(posterTitle);
+	  poster.appendChild(posterImg);
+	  posterTitle.textContent = e.title;
+	  posterOverview.textContent = e.overview;
+	  poster.appendChild(posterOverview);
+	  result.appendChild(poster);
     }));
 });
