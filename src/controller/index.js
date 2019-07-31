6ef0ec2cf837845976180b9e0ebf44b96275def7
mossa-Sammer <mossa708496@gmail.com>
2019-07-31 16:30:18 +0300

    make the error testing relates #13

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const search = require('./search');
 
+const { client, server } = require('./error');
+
 const router = express.Router();
 
 router.post('/search', search.post);
+
+router.use(client);
+
+router.use(server);
 
 module.exports = router;
