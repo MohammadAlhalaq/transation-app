cd1af3fa7959acfa90aff2d6899b2efb015c4c1f
fadi omar <fadeomar2015@gmail.com>
2019-07-31 10:35:20 +0300

    edit dom file and add the correct endpoint

--- public/js/dom.js
+++ public/js/dom.js
@@ -9,7 +9,7 @@ const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
   const word = input.value;
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=e60d1f76001c4655e361864c383b4640&query=${word}&page=1&include_adult=false`)
+  fetch(`/search/:${word}`)
     .then(res => res.json())
     .then(res => res.results)
     .then(res => res.forEach((e) => {
